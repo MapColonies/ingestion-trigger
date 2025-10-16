4f1d339e070932a078f129d4b855e7ebd390f44d
vitaligi <54726763+vitaligi@users.noreply.github.com>
2025-10-17 00:46:20 +0300

    test: add utility types

--- tests/utils/types.ts
+++ tests/utils/types.ts
@@ -1,5 +1,13 @@
 export type DeepPartial<T> = T extends object
   ? {
-      [P in keyof T]?: T[P] extends object ? DeepPartial<T[P]> : T[P];
+      [K in keyof T]?: T[K] extends object ? DeepPartial<T[K]> : T[K];
     }
   : T;
+
+export type FlatRecordValues<T> = {
+  [K in keyof T as T[K] extends unknown[] ? (K extends `${infer Q}s` ? Q : K) : K]: T[K] extends (infer U)[] ? U : T[K];
+};
+
+export type ReplaceValueWithFunctionResponse<T extends object> = {
+  [K in keyof T]: (...args: any[]) => T[K];
+};
