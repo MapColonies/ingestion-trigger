dc2c9fe066d5e5491956a7f0539449d4b8d9aad8
vitaligi <54726763+vitaligi@users.noreply.github.com>
2025-11-11 13:19:37 +0200

    refactor: remove unused isPixelSizeValid function

--- src/utils/pixelSizeValidate.ts
+++
@@ -1,5 +0,0 @@
-export const isPixelSizeValid = (requestedPixelSize: number, sourcePixelSize: number, resolutionFixedPointTolerance: number): boolean => {
-  const fixedRequestedPixelSize = parseFloat(requestedPixelSize.toFixed(resolutionFixedPointTolerance));
-  const fixedSourcePixelSize = parseFloat(sourcePixelSize.toFixed(resolutionFixedPointTolerance));
-  return fixedRequestedPixelSize >= fixedSourcePixelSize;
-};
