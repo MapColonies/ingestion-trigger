225c0e910cc203a52177f6766f00705e14f28e70
razbroc <razbroc@gmail.com>
2025-12-08 10:59:51 +0200

    refactor: remove unused checksumSchema import from interfaces

--- src/ingestion/interfaces.ts
+++ src/ingestion/interfaces.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-magic-numbers */
 import { ICreateJobResponse } from '@map-colonies/mc-priority-queue';
-import { ingestionBaseJobParamsSchema, ingestionValidationTaskParamsSchema, checksumSchema, Checksum } from '@map-colonies/raster-shared';
+import { ingestionBaseJobParamsSchema, ingestionValidationTaskParamsSchema, Checksum } from '@map-colonies/raster-shared';
 import z from 'zod';
 
 export interface SourcesValidationResponse {
